edd74574d572ebb105d3c52c4c3c1b3dd8731718
Eduardo <edupavanatti@gmail.com>
2020-02-26 20:41:36 -0300

    Buttons task updated commit.

--- app/src/main/java/com/example/weeklychallenge/MainActivity.java
+++ app/src/main/java/com/example/weeklychallenge/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.weeklychallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createButton(R.id.buttonA);
-        createButton(R.id.buttonB);
-    }
-
-    /**
-     * Creates a pre-defined button model with layout based on the ID received by parameter.
-     *
-     * @param buttonID the ID of the button layout to be created.
-     */
-    public void createButton(int buttonID) {
-        Button button = findViewById(buttonID);
-        button.setText(R.string.button_text);
-        button.setTextColor(Color.rgb(80, 214, 145));
-        button.setBackgroundColor(Color.rgb(71, 29, 124));
     }
 }
